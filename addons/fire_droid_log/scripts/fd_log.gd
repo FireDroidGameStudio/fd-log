ba4e099a87c79006d0f6f194ab1ead9b4008d284
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2025-07-06 17:36:35 -0300

    feat: Add log level enable checks and autoload for FDLog
    Introduced functions to check if a log level is enabled before logging messages in fd_log.gd. Updated log_message to respect these settings. Also added FDLog as an autoload singleton in project.godot for global access.

--- addons/fire_droid_log/scripts/fd_log.gd
+++ addons/fire_droid_log/scripts/fd_log.gd
@@ -35,7 +35,16 @@ func _physics_process(_delta: float) -> void:
 	pass
 
 
+func get_enable_log_level_setting_path(level: LogLevel) -> String:
+	const LOG_LEVEL_SETTING_PATH: String = "FDLog/log_level/"
+	return (
+		LOG_LEVEL_SETTING_PATH + "enable_" + LogLevel.keys()[level].to_lower()
+	)
+
+
 func log_message(message: String, level: LogLevel = LogLevel.INFO) -> void:
+	if not is_level_enabled(level):
+		return
 	var timestamp: String = Time.get_time_string_from_system()
 	var color: String = Colors.get(level, "gray")
 	if level == LogLevel.WARNING:
@@ -43,3 +52,17 @@ func log_message(message: String, level: LogLevel = LogLevel.INFO) -> void:
 	if level == LogLevel.ERROR:
 		push_error("[%s]: %s" % [timestamp, message])
 	print_rich("[color=%s][%s]: %s[/color]" % [color, timestamp, message])
+
+
+func can_print_log_level() -> bool:
+	return (
+		ProjectSettings.get_setting(get_print_log_level_setting_path(), true)
+	)
+
+
+func is_level_enabled(level: LogLevel) -> bool:
+	return ProjectSettings.get_setting(
+		get_enable_log_level_setting_path(level), true
+	)
+
+
